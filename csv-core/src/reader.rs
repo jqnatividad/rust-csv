dfa9c3dca3f88005f54ae1072daed6bd29720a1e
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2024-09-29 09:05:30 -0400

    make clippy happy

--- csv-core/src/reader.rs
+++ csv-core/src/reader.rs
@@ -439,7 +439,7 @@ enum NfaState {
 }
 
 /// A list of NFA states that have an explicit representation in the DFA.
-const NFA_STATES: &'static [NfaState] = &[
+const NFA_STATES: &[NfaState] = &[
     NfaState::StartRecord,
     NfaState::StartField,
     NfaState::EndFieldDelim,
@@ -610,11 +610,11 @@ impl Reader {
     /// this method will fail to strip off the BOM if only part of the BOM is
     /// buffered. Hopefully that won't happen very often.
     fn strip_utf8_bom<'a>(&self, input: &'a [u8]) -> (&'a [u8], usize) {
-        let (input, nin) = if {
+        let (input, nin) = if 
             !self.has_read
                 && input.len() >= 3
                 && &input[0..3] == b"\xef\xbb\xbf"
-        } {
+        {
             (&input[3..], 3)
         } else {
             (input, 0)
